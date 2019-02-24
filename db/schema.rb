01a0b7ae182ef905b4e82a7427b9573f15dc7b6c
Julia Marco <marcootj@gmail.com>
2019-02-24 10:09:57 -0700

    set up coupons table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190210231817) do
+ActiveRecord::Schema.define(version: 20190224170429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "code"
+    t.boolean "percentage", default: false
+    t.float "dollar", default: 0.0
+    t.boolean "active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_coupons_on_merchant_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -45,6 +56,8 @@ ActiveRecord::Schema.define(version: 20190210231817) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "coupon_id"
+    t.index ["coupon_id"], name: "index_orders_on_coupon_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -65,8 +78,10 @@ ActiveRecord::Schema.define(version: 20190210231817) do
     t.index ["state"], name: "index_users_on_state"
   end
 
+  add_foreign_key "coupons", "users", column: "merchant_id"
   add_foreign_key "items", "users", column: "merchant_id"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "coupons"
   add_foreign_key "orders", "users"
 end
